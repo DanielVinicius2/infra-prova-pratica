9688cf999564e99eb23c94617668c060747f76a7
Daniel <daniel.vsobralv3@gmail.com>
2025-10-26 23:34:05 -0300

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  type = string
+  description = "Nome do bucket (must be globally unique)."
+  default = "infra-prova-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
